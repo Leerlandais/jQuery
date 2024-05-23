245534c012b1d727c8f2e5050c00da73e4746ba9
leerlandais <leerlandais@gmail.com>
2024-05-23 08:16:27 +0200

    pre-school push

--- query.js
+++ query.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function(){       // maybe not needed but does the same as document.addEventListener("DOMContentLoaded"...)
 
-    $("h1").fadeIn(2000);
+    $("h1").fadeIn(2000);       // just because
     
     $(".testButtons").hover( function(){
         $(this).css("opacity", "1")
     },
     function() {
         $(this).css("opacity", "0.5")
-    });
+    });         // I wanted a way to handle mouseenter and mouseleave in a toggle fashion. This is the best I could manage
 
-    $(".firstTestButton").on("click", function() {
+    $(".firstTestButton").on("click", function() { // yeah, yeah, too easy
         $(".firstTestButton").parent().fadeOut(3000); 
         $(".firstTestButton").parent().fadeIn(1000); 
     });
@@ -56,8 +56,9 @@ $(document).ready(function(){
         }, 5000);
     });
 
-    $(".fifthTestButton").one('mouseenter',function() {
+    $(".fifthTestButton").one('mouseenter',function() { // nice! same as .on but only do it once. jQ thought of everything (I was going to write a function to handle that)
         const parent = $(this).parent();
+        $(".fifthTestButton").hide();
             parent.find("h4").text("What would you like?");
             parent.find("h4").append("<br><button>Fade Things?</button>");
             parent.find("h4").append("<br><button>Replace Things?</button>");
